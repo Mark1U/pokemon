bf4c7e08ced182f7d963807bab8c15f424011e98
Mark1U <mark@zurcloud.com>
2024-11-14 14:27:49 +0100

    Add: Modal

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ import { typeColor } from "./constants/typeColor";
 const searchInput = document.querySelector("#searchInput") as HTMLInputElement;
 const buttonDiv = document.querySelector("#buttonDiv") as HTMLDivElement;
 const output = document.querySelector("#output") as HTMLDivElement;
+const modal = document.getElementById("pokemonModal");
+const pokemonData = document.getElementById("pokemonData");
+const span = document.getElementsByClassName("close")[0];
+const searchCount = document.getElementById('searchCount')
 
 const BASE_URL: string = "https://pokeapi.co/api/v2";
 const allPokemonArr: IPokemonInfo[] = [];
@@ -20,9 +24,8 @@ async function fetchPokemon(limit: number, offset: number) {
     const fetchPromises = apiresult.results.map((pokemon: IResult) => fetchPokemonDetails(pokemon.url));
     await Promise.all(fetchPromises);
     console.log({ storeCounter, fetchCounter })
-    console.log(fetchPromises);
-    console.log(allPokemonArr);
     displayPokemon(allPokemonArr);
+
 }
 
 let storeCounter = 0
@@ -47,21 +50,41 @@ async function fetchPokemonDetails(url: string) {
 
 function displayPokemon(pokemonArr: IPokemonInfo[]) {
     output.innerHTML = "";
-    pokemonArr.slice(0, 96).forEach((pokemon) => {
-        console.log("innerhalb der funktion");
-        output.innerHTML += `
-        <article>
+    searchCount!.textContent = `${pokemonArr.length}/${allPokemonArr.length}`
+
+    pokemonArr.slice(0, 200).forEach((pokemon) => {
+
+        const article = document.createElement('article')
+        article.innerHTML = `
             <div class="img-container">
                 <img src="${pokemon.imgUrl}" class="pokemon-img">
             </div>
             <div class="info-container">
                 <p>#${pokemon.id.toString().padStart(3, "0")}</p>
                 <p>${pokemon.name}</p>
-            </div>
-        </article>
-        `;
-    });
+            </div>`;
+
+        article.addEventListener('click', () => {
+            pokemonData!.innerHTML = ` <div class="img-container">
+    <img src="${pokemon.imgUrl}" class="pokemon-img">
+</div>
+<div class='typeBtnNav'>
+    <button style='color:white;background-color:${typeColor.get(pokemon.types[0]) || "#fff"}' id='btnType1' class='button-type' >${pokemon.types[0] || "-"}</button>
+    <button style='color:white;background-color:${typeColor.get(pokemon.types[1]) || "#fff"}'id='btnType2' class='button-type' >${pokemon.types[1] || "-"}</button>
+</div>
+<div class="info-container">
+    <p>#${pokemon.id.toString().padStart(3, "0")}</p>
+    <p>${pokemon.name}</p>
+</div>`
+
+            modal!.style.display = "block";
+        })
+
+
+        output.appendChild(article)
+    })
 }
+
 
 async function fetchTypes() {
     const response = await fetch(`${BASE_URL}/type`);
@@ -79,6 +102,7 @@ async function fetchTypes() {
         button.className = "button-type";
         button.style.backgroundColor = typeColor.get(typeName) || "#777";
         button.addEventListener("click", () => {
+            searchInput.value = "";
             const modifiedArr = allPokemonArr.filter((pokemon) => pokemon.types.includes(typeName));
             displayPokemon(modifiedArr);
         });
@@ -90,6 +114,17 @@ searchInput.addEventListener("input", () => {
     const modifiedArr = allPokemonArr.filter((pokemon) => pokemon.name.toLowerCase().includes(searchInput.value.trim().toLowerCase()));
     displayPokemon(modifiedArr);
 });
+
+span.addEventListener('click', () => {
+    modal!.style.display = "none";
+})
+
+window.addEventListener('click', (event) => {
+    if (event.target == modal) {
+        modal!.style.display = "none";
+    }
+})
+
 
 fetchTypes();
 fetchPokemon(1509, 0);
